b8c9de05d443895426f7abcc536e3de4f9e01ebf
Shalini <shaliniaralimar24@gmail.com>
2026-01-04 22:06:45 +0530

    Add functions to calculate areas of shapes

---
+++ myproject/one.py
@@ -0,0 +1,11 @@
+def circle(r):
+    print("Area of circle is:",3.14*r*r)
+    
+def triangle(b,h):
+    print("Area of triangle is:",0.5*b*h)  
+    
+def square(s):
+    print("Area of square is:",s*4)  
+    
+def rectangle(l,b):
+    print("Area of rectangle is:",l*b)
